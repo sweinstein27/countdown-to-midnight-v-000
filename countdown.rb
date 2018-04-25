7c454280b2762ce85f8561215ba42155adb87035
Scott Weinstein <sweinstein27@gmail.com>
2018-04-25 14:13:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 integer = 10
 def countdown(integer)
-  until integer == 0
-    integer - 1
+  while integer !== 0
+    integer -= 1
     puts "#{integer} SECOND(S)!"
   end
   puts "HAPPY NEW YEAR!"
