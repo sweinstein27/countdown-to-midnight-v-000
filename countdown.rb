a90b05fd75f7d98f5dca3ef2893c0785b4a86884
Scott Weinstein <sweinstein27@gmail.com>
2018-04-25 14:09:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,14 @@
 #write your code here
 integer = 10
 
-def midnight?
+def midnight?(integer)
   until integer = 0
     integer - 1
     puts "#{integer} SECOND(S)!"
   end
   puts "HAPPY NEW YEAR!"
   end
-  
+
 
 
 
