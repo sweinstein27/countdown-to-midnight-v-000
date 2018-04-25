4d7062494fca64cff1b6ca2944251135ef50e3f4
Scott Weinstein <sweinstein27@gmail.com>
2018-04-25 14:48:55 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(integer)
   end
   return "HAPPY NEW YEAR!"
   end
+
+def countdown_with_sleep(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1
+    sleep 1
+  end
+end
