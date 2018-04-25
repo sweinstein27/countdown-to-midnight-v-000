c74971a0fa554b22a4828c8c972a76d95b329d0b
Scott Weinstein <sweinstein27@gmail.com>
2018-04-25 14:09:19 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+integer = 10
+
+def midnight?
+  until integer = 0
+    integer - 1
+    puts "#{integer} SECOND(S)!"
+  end
+  puts "HAPPY NEW YEAR!"
+  end
+  
+
+
+
 
 def countdown
 end
